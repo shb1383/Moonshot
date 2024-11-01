994f43620f2eccca4619d35bb096276fcf28baba
syrene <144994323+shb1383@users.noreply.github.com>
2024-11-01 23:49:58 +0900

    added json files and images, and created additional swift files

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Syrene Haber Bartolome on 2024/11/01.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
